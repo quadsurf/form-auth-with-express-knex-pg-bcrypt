17a6003928d287c177f0bd013812456de5d221d4
CJ R <hello@cjr.co.de>
2016-01-11 23:23:07 -0700

    remove session and cookie-parser

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var auth = require('./routes/auth');
 var users = require('./routes/users');
@@ -18,23 +16,13 @@ require('dotenv').load();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-  secret: process.env.SECRET,
-  resave: true,
-  saveUninitialized: true
-}));
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   next();
 });
 
 app.use(auth.passport.initialize());
-app.use(auth.passport.session());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', auth.router);
 app.use('/users', users);
